4f63220f16e4120daaeafae7d89426e9ff0a268a
yash-levi1896 <yashkumar18gupta@gmail.com>
2023-09-22 11:27:10 +0530

    created registered and login route

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const  connection  = require('./config/db');
+const {userRoute}=require('./Routes/user.route')
+require('dotenv').config()
+const app=express()
+
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send("hello")
+})
+app.use("/user",userRoute);
+
+
+
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection
+        console.log("connected to db")
+    } catch (error) {
+        console.log(error.message)
+    }
+    console.log("server is running")
+})
